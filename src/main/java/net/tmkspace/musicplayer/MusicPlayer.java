0d0c48f781ce84c88ee0efc33fd7bb8cf06ad25b
tikenshot <73959581+tikenshot@users.noreply.github.com>
2025-05-30 12:12:58 +0300

    some shit

--- src/main/java/net/tmkspace/musicplayer/MusicPlayer.java
+++ src/main/java/net/tmkspace/musicplayer/MusicPlayer.java
@@ -1,17 +1,50 @@
 package net.tmkspace.musicplayer;
 
+import de.maxhenkel.voicechat.api.BukkitVoicechatService;
+import github.scarsz.configuralize.DynamicConfig;
+import github.scarsz.configuralize.ParseException;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
 
 public final class MusicPlayer extends JavaPlugin {
 
+    private final DynamicConfig config;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        BukkitVoicechatService service = getServer().getServicesManager().load(BukkitVoicechatService.class);
+        if (service != null) {
+            service.registerPlugin(new MusicPlayerVoicePlugin());
+        }
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+    }
+
+    public static MusicPlayer getPlugin() {
+        return getPlugin(MusicPlayer.class);
+    }
+
+    public static DynamicConfig config() {
+        return getPlugin().config;
+    }
+
+    public void reloadConfig() {
+        try {
+            config().loadAll();
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException("Failed to load config: ", e);
+        }
+    }
+
+    public MusicPlayer() {
+        super();
+
+        getDataFolder().mkdirs();
+        config = new DynamicConfig();
     }
 }
---
+++ src/main/java/net/tmkspace/musicplayer/MusicPlayerVoicePlugin.java
@@ -0,0 +1,11 @@
+package net.tmkspace.musicplayer;
+
+import de.maxhenkel.voicechat.api.VoicechatPlugin;
+
+public class MusicPlayerVoicePlugin implements VoicechatPlugin {
+
+    @Override
+    public String getPluginId() {
+        return "music_player";
+    }
+}
